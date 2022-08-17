56b224a95cb3fe82fcd7d0b17f5db1ddf32a478e
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-17 14:15:41 +0500

    hello

--- bin/list_method10.dart
+++ bin/list_method10.dart
@@ -7,4 +7,30 @@
         list: return answer
 */
 
-void main() {}
+List func(List numbers) {
+  int i = 0;
+  int x = 0;
+  int g = 0;
+  List t = [];
+  List t1 = [];
+  List t2 = [];
+  while (i < numbers.length) {
+    if (numbers[i] == 0) {
+      x += 1;
+      i++;
+    } else if(numbers[i] == 1) {
+      g+=1;
+      i++;
+    }
+  }
+  t1.add(x);
+  t2.add(g);
+  
+  
+
+  return t1+t2;
+}
+
+void main() {
+  print(func([1, 0, 1, 1, 0, 1, 1]));
+}
